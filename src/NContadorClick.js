5d04d6877ac740d67bacb209bfba97bec78708fc
Bruno Vitor - Notebook <bvpn20@gmail.com>
2023-10-14 04:58:53 -0300

    adicionando o uso do useState

--- src/NContadorClick.js
+++ src/NContadorClick.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ContadorClick = () => {
     // Usando o hook 'useState' para criar uma variável de estado 'contador' e uma função 'setContador'
     const [contador, setContador] = useState(0);
+
+    useEffect(() => { //sempre altera quando o valor de 'contador' mudar...
+        document.title = `Count: ${contador}`;
+      }, [contador]);//lista de dependências
 
     // Função para incrementar o contador
     const contar = () => {
